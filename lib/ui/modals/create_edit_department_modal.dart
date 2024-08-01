2f63d0d83216502412eedfa8a6334e638631f1c7
Shinan Tc <shinantc1@gmail.com>
2024-08-01 22:59:01 +0530

    margin and subtitle added

--- lib/ui/modals/create_edit_department_modal.dart
+++ lib/ui/modals/create_edit_department_modal.dart
@@ -1,47 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CreateEditDepartmentModal extends StatelessWidget {
   const CreateEditDepartmentModal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // for getting the screen height (used for making the modal to take majority of the screen height)
-    final screenHeight = (MediaQuery.of(context).size.height) - 100;
+    // for getting the screen height (used for making the modal to take majority o the screen height)
+    // final screenHeight = (MediaQuery.of(context).size.height) - 100;
 
     return SingleChildScrollView(
-      child: Container(
-        height: screenHeight,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'Create / Edit Department',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+            child: Text(
+              "Create / Edit Department",
               textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
             ),
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Name',
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 0, 30, 30),
+            child: Container(
+              width: 400,
+              height: 550,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Color.fromARGB(255, 244, 241, 241),
+                  borderRadius: BorderRadius.zero,
+                  border: Border(
+                      top: BorderSide(color: Colors.black),
+                      left: BorderSide(color: Colors.black))),
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Handle save button click
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
