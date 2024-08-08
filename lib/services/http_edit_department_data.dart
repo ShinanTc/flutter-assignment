3e8301c14d4df68e465c297f06f32218a8bb11bc
Shinan Tc <shinantc1@gmail.com>
2024-08-08 11:46:33 +0530

    edit_department_data.dart file created for patch request

---
+++ lib/services/http_edit_department_data.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiPatchService {
+  final String baseUrl = 'http://127.0.0.1:8000';
+
+  Future<void> sendPatchRequest(String data, int departmentId) async {
+    final url = Uri.parse('$baseUrl/department/edit-department');
+
+    final response = await http.patch(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+        <String, String>{
+          'data': data,
+          'department_id': departmentId.toString()
+        },
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      print(
+          '--------------------- PATCH request successful! --------------------- ');
+    } else {
+      print('Error sending POST request: ${response.statusCode}');
+    }
+  }
+}
